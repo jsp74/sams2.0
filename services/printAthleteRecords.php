fc2b28b673f6367e75bdd942fe5b296a6ed7b3b1
Jay Patel <1jaycpatel1@gmail.com>
2019-10-23 14:12:38 -0400

    reading data.csv from printAthleteService

--- services/printAthleteRecords.php
+++ services/printAthleteRecords.php
@@ -15,22 +15,15 @@ gatekeeper("../login/login.php");
     <title>Document</title>
 </head>
 <body>
-    <table style="width:100%">
-    <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Age</th>
-    </tr>
-    <tr>
-        <td>Jill</td>
-        <td>Smith</td>
-        <td>50</td>
-    </tr>
-    <tr>
-        <td>Eve</td>
-        <td>Jackson</td>
-        <td>94</td>
-    </tr>
-    </table>
+    <?php
+        $file = fopen("../masterpage/data.csv") or die("unable to open file");
+        $data = fgetcsv($file);
+        for($i = 0; $i < count($data); $i++){
+            echo `
+            <th scope = 'col'> <p class='text-center'>{{$data[$i]}}</p></th>
+            `
+        }
+        fclose($file);
+    ?>
 </body>
 </html>
